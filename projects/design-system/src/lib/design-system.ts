052297c0c9271b35985b6082043512810f3722d2
kakon-selise <kakon.mehedi@selisegroup.com>
2026-07-25 19:05:26 +0600

    add design system ready for release 1

---
+++ projects/design-system/src/lib/design-system.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kart-design-system',
+  imports: [],
+  template: ` <p>design-system works!</p> `,
+  styles: ``,
+})
+export class DesignSystem {}
